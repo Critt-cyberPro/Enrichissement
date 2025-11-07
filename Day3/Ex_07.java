ed27566568aa53b43cf3a0277ef945dff8374584
CrittMatt <lolipop1dj@gmail.com>
2025-11-07 13:59:08 -0500

    J1 + J2 + J3(8)
    7 fini !!!

--- Day3/Ex_07.java
+++ Day3/Ex_07.java
@@ -3,9 +3,9 @@ import java.util.Random;
 public class Ex_07 {
 public static void main(String[] args) {
     int [] iTableau = new int[10];
-    Object [][] iClassement = new Object[2][5];
     Random rNombre = new Random();
     int iNombre;
+    System.out.println("");
     int i0=0,i1=0,i2=0,i3=0,i4=0;
     for ( int i=0; i< iTableau.length ; i++) {
         iNombre = rNombre.nextInt(5);
@@ -28,6 +28,23 @@ public static void main(String[] args) {
                 i4++;
             }
     }
+    Object [][] iClassement = {
+
+        {"Nombre 0","Nombre 1","Nombre 2","Nombre 3","Nombre 4"},
+        {i0,i1,i2,i3,i4}
+    };
+    Classement(iClassement);
+    System.out.println(); 
+}
+public static void Classement(Object [][] iClassement) {
     
+    for (int i = 0; i < iClassement.length; i++) {
+
+        for (int j = 0; j < iClassement[i].length; j++) {
+            System.out.print(iClassement[i][j] + "     ");
+        }
+        System.out.println(); 
+    }
 }
 }
+
