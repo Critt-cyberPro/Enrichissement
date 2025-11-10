00261d856b5528f2eb754428577e884571e2ba7f
CrittMatt <lolipop1dj@gmail.com>
2025-11-10 01:11:29 -0500

    J1 + J2 + J3(8)
    8 commencer mais pas beaucoup

--- Day3/Ex_08.java
+++ Day3/Ex_08.java
@@ -1,5 +1,19 @@
+import java.util.Random;
+
 public class Ex_08 {
 public static void main(String[] args) {
-    
+    Random lire = new Random();
+
+    Object [][] iNote = {
+
+        {"Étudiants","Pierre","André","Julie","Brigitte","Anne","Olivier","Simon","Nadine"},
+        {"TP1",65,78,43,82,76,91,71,96},
+        {"TP2",45,55,67,89,100,95,92,81},
+        {"Examen1",34,65,21,67,75,54,69,54}
+    };
+    moyEval(iNote);
+}
+public static void moyEval(Object [][] iNote) {
+    iNote[2][2]
 }
 }
