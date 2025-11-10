6553c2e04f7ecca5964e2b2278e49f74771e4e02
CrittMatt <lolipop1dj@gmail.com>
2025-11-10 16:13:58 -0500

    J1+J2+J3+J4(Début)
    j'ai commencer le premier même si je ne le comprend pas encore parfaitement

--- Day4/Ex_01.java
+++ Day4/Ex_01.java
@@ -2,16 +2,37 @@
 public class Ex_01 {
 public static void main(String[] args) {
 
-    String [] sTableu = new String[5];
+    int [] iTableu = new int[8];
+    iTableu[0] = 8;
+    iTableu[1] = 7;
+    iTableu[2] = 4;
+    iTableu[3] = 9;
+    iTableu[4] = 6;
+    iTableu[5] = 5;
+    iTableu[6] = 2;
+    iTableu[7] = 1;
+    iTableu[8] = 3;
+    
 
-    sTableu[0] = "Salut" ;
-    sTableu[1] = "j'ai" ;
-    sTableu[2] = "plein" ;
-    sTableu[3] = "de" ;
-    sTableu[4] = "patates !" ;
+}
+public static int[][] triller(int [] iTableu) {
 
-    for (int i=0; i < sTableu.length ; i++) {
-        System.out.print(sTableu[i]+ " ");
+    Object [3][3] iTableu2;
+
+
+
+
+    
+    return iTableu2;
+}
+public static void Classement(Object [][] iTableu2) {
+    
+    for (int i = 0; i < iTableu2.length; i++) {
+
+        for (int j = 0; j < iTableu2[i].length; j++) {
+            System.out.print(iTableu2[i][j] + "     ");
+        }
+        System.out.println(); 
     }
 }
 }
